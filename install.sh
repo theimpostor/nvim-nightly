adefdb7e339ec04ca6ae2fffd2df64f2ab89921f
theimpostor <sahirhoda@gmail.com>
2021-11-15 10:34:13 -0600

    add install script

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 set -euf -o pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mkdir -p "$SCRIPT_DIR/.config"
+mkdir -p "$SCRIPT_DIR/.local/share"
+mkdir -p "$SCRIPT_DIR/.local/state"
+mkdir -p "$SCRIPT_DIR/.cache"
 NVIMAPPIMG="$SCRIPT_DIR/nvim.appimage"
 NVIM="$SCRIPT_DIR/nvim"
 
@@ -11,6 +16,7 @@ set -x
 case $(uname -s) in
     Darwin)
         curl -fsSL https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz | tar xzf -
+        ln -s nvim-osx64/bin/nvim "$NVIMAPPIMG"
         ;;
 
     *)
@@ -19,4 +25,6 @@ case $(uname -s) in
         ;;
 esac
 
-"$NVIM" +PlugUpgrade +qa && "$NVIM" +PlugUpdate
+# install vim  plug
+curl -fsSLo "$SCRIPT_DIR/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+"$NVIM" +PlugInstall
