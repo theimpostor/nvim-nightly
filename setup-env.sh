ad4b45f4d488550edc70fbf470ff83034c781ef6
theimpostor <sahirhoda@gmail.com>
2021-12-20 09:27:36 -0600

    better comments

--- setup-env.sh
+++ setup-env.sh
@@ -4,8 +4,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # https://stackoverflow.com/a/28776166
 if (return 0 2>/dev/null) ; then
+    # sourced
     export PATH="$SCRIPT_DIR/bin:$PATH"
 else
-    # usage: eval "$(./setup-env.sh)"
+    # usage: source <(./setup-env.sh)
     echo export PATH=\""$SCRIPT_DIR/bin:\$PATH"\"
 fi
